03e0097bca5a32ab66a01aed21a9770f971619f7
Nurtak <nurtak@gmail.com>
2013-03-04 11:21:09 +0100

    hello world

---
+++ src/Start.java
@@ -0,0 +1,11 @@
+public class Start {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        System.out.println("hello world");
+    }
+
+}
