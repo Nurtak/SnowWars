4120ccfd37b7e2f89ee62fd34d916403d42ac8a8
flae <freinhar@hsr.ch>
2013-03-04 16:00:01 +0100

    Test Reinhard

--- src/Start.java
+++ src/Start.java
@@ -6,6 +6,7 @@ public class Start {
     public static void main(String[] args) {
         // TODO Auto-generated method stub
         System.out.println("hello world");
+        int x = 3;
         
     }
 
