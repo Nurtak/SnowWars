660277b8bc5db2042620525360826e7f06126d83
flae <freinhar@hsr.ch>
2013-04-12 14:33:07 +0200

    Run changed

--- src/ch/hsr/se2p/snowwars/application/Run.java
+++ src/ch/hsr/se2p/snowwars/application/Run.java
@@ -1,17 +1,39 @@
 package ch.hsr.se2p.snowwars.application;
 
+import org.apache.log4j.ConsoleAppender;
+import org.apache.log4j.Logger;
+import org.apache.log4j.PatternLayout;
+import org.apache.log4j.RollingFileAppender;
+
+
 public class Run {
 
-    /**
-     * @param args
-     *            "-server" for starting the server
-     */
-    public static void main(String[] args) {
-        if (args.length != 0 && "-server".equalsIgnoreCase(args[0])) {
-            new RunSnowWarsServer();
-        } else {
-            new RunSnowWarsClient();
-        }
+	/**
+	 * @param args
+	 *           "-server" for starting the server
+	 */
+	public static void main(String[] args) {
+		installLogger();
+		if (args.length != 0 && "-server".equalsIgnoreCase(args[0])) {
+			new RunSnowWarsServer().startProgram();
+		} else {
+			new RunSnowWarsClient().startProgram();
+		}
+	}
 
-    }
+	/**
+	 * Installs Log4j-Library
+	 */
+	public static void installLogger() {
+		try {
+			Logger root = Logger.getRootLogger();
+			PatternLayout layout = new PatternLayout("|%-32.32F|%-6p| %m%n");
+			String logfilePath = "snowWarsLogfile.log";
+			RollingFileAppender newrfa = new RollingFileAppender(layout, logfilePath);
+			root.addAppender(newrfa);
+			root.addAppender(new ConsoleAppender(layout));
+		} catch (Exception e) {
+			System.out.println("ERROR: Could not install logger!" + e.getMessage());
+		}
+	}
 }
