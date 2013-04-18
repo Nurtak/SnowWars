9688a4da8a2df2164824e2dc945bc4d1da2330a3
Nurtak <nurtak@gmail.com>
2013-04-18 13:04:24 +0200

    commit fix

--- src/ch/hsr/se2p/snowwars/config/SnowWarsConfig.java
+++ src/ch/hsr/se2p/snowwars/config/SnowWarsConfig.java
@@ -2,40 +2,40 @@ package ch.hsr.se2p.snowwars.config;
 
 public class SnowWarsConfig {
 
-	private String hostname;
-	private String rmiLookupName;
-	private int rmiRegistryPort;
-	private int rmiRemotePort;
+    private String hostname;
+    private String rmiLookupName;
+    private int rmiRegistryPort;
+    private int rmiRemotePort;
 
-	public String getHostname() {
-		return hostname;
-	}
+    public String getHostname() {
+        return hostname;
+    }
 
-	public void setServerHostname(String serverHostname) {
-		this.hostname = serverHostname;
-	}
+    public void setServerHostname(String serverHostname) {
+        this.hostname = serverHostname;
+    }
 
-	public int getRmiRegistryPort() {
-		return rmiRegistryPort;
-	}
+    public int getRmiRegistryPort() {
+        return rmiRegistryPort;
+    }
 
-	public void setRmiRegistryPort(int rmiRegistryPort) {
-		this.rmiRegistryPort = rmiRegistryPort;
-	}
+    public void setRmiRegistryPort(int rmiRegistryPort) {
+        this.rmiRegistryPort = rmiRegistryPort;
+    }
 
-	public int getRmiRemotePort() {
-		return rmiRemotePort;
-	}
+    public int getRmiRemotePort() {
+        return rmiRemotePort;
+    }
 
-	public void setRmiRemotePort(int rmiRemotePort) {
-		this.rmiRemotePort = rmiRemotePort;
-	}
+    public void setRmiRemotePort(int rmiRemotePort) {
+        this.rmiRemotePort = rmiRemotePort;
+    }
 
-	public String getServerRMILookupName() {
-		return rmiLookupName;
-	}
+    public String getServerRMILookupName() {
+        return rmiLookupName;
+    }
 
-	public void setServerRMILookupName(String serverRMILookupName) {
-		this.rmiLookupName = serverRMILookupName;
-	}
+    public void setServerRMILookupName(String serverRMILookupName) {
+        this.rmiLookupName = serverRMILookupName;
+    }
 }
