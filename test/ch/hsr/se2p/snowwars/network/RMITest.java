b919388a34990f804652395e485499deb631ca41
Nurtak <nurtak@gmail.com>
2013-04-22 14:20:51 +0200

    testing

--- test/ch/hsr/se2p/snowwars/network/RMITest.java
+++ test/ch/hsr/se2p/snowwars/network/RMITest.java
@@ -1,5 +1,7 @@
 package ch.hsr.se2p.snowwars.network;
 
+import static org.junit.Assert.fail;
+
 import java.io.IOException;
 import java.net.ServerSocket;
 
@@ -19,39 +21,49 @@ public class RMITest {
     public static void setUpConfig() {
         snowWarsConfig = ConfigFactory.getSnowWarsConfig();
     }
-
-    @Test(expected = IOException.class)
-    public void testServerport() throws SnowWarsRMIException, IOException {
-        new RunRMIServer(new SnowWarsServer());
+    
+    @Test
+    public void testRegistryPortIsNotInUse() throws SnowWarsRMIException, IOException {
         ServerSocket serverSocket = new ServerSocket(snowWarsConfig.getRmiRegistryPort());
-        // Port should be already in use
         serverSocket.close();
     }
 
-//    @Test
-//    public void testRMICommunication() {
-//
-//        try {
-//            SnowWarsServer snowWarsServer = new SnowWarsServer();
-//            RunRMIServer runRMIServer = new RunRMIServer(snowWarsServer);
-//
-//            Registry serverRegistry = LocateRegistry.getRegistry(snowWarsConfig.getHostname(), snowWarsConfig.getRmiRegistryPort());
-//
-//            SnowWarsClient snowWarsClient = new SnowWarsClient();
-//            RunRMIClient runRMIClient = new RunRMIClient(snowWarsClient);
-//
-//            RMIClientInterface client = new RMIClient(runRMIClient);
-//            RMIClientInterface clientStub = (RMIClientInterface) UnicastRemoteObject.exportObject(client, 0);
-//
-//            RMIServerInterface server = (RMIServerInterface) serverRegistry.lookup(snowWarsConfig.getServerRMILookupName());
-//            // server.registerClient(clientStub);
-//            // server.deregisterClient(clientStub);
-//
-//        } catch (SnowWarsRMIException | RemoteException | NotBoundException e) {
-//            e.printStackTrace();
-//            fail();
-//        }
-//
-//    }
+    @Test(expected = IOException.class)
+    public void testServerUsesRightRegistryPort() throws SnowWarsRMIException, IOException {
+        new RunRMIServer(new SnowWarsServer());
+        ServerSocket serverSocket = new ServerSocket(snowWarsConfig.getRmiRegistryPort());
+        serverSocket.close();
+    }
+    
+
+    // @Test
+    // public void testRMICommunication() {
+    //
+    // try {
+    // SnowWarsServer snowWarsServer = new SnowWarsServer();
+    // RunRMIServer runRMIServer = new RunRMIServer(snowWarsServer);
+    //
+    // Registry serverRegistry =
+    // LocateRegistry.getRegistry(snowWarsConfig.getHostname(),
+    // snowWarsConfig.getRmiRegistryPort());
+    //
+    // SnowWarsClient snowWarsClient = new SnowWarsClient();
+    // RunRMIClient runRMIClient = new RunRMIClient(snowWarsClient);
+    //
+    // RMIClientInterface client = new RMIClient(runRMIClient);
+    // RMIClientInterface clientStub = (RMIClientInterface)
+    // UnicastRemoteObject.exportObject(client, 0);
+    //
+    // RMIServerInterface server = (RMIServerInterface)
+    // serverRegistry.lookup(snowWarsConfig.getServerRMILookupName());
+    // // server.registerClient(clientStub);
+    // // server.deregisterClient(clientStub);
+    //
+    // } catch (SnowWarsRMIException | RemoteException | NotBoundException e) {
+    // e.printStackTrace();
+    // fail();
+    // }
+    //
+    // }
 
 }
