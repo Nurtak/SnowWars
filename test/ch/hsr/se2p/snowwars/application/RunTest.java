e9bde3c175f30757184698bf67801c5d5a3f523b
flae <freinhar@hsr.ch>
2013-04-22 16:46:44 +0200

    asdfasdfasdf

--- test/ch/hsr/se2p/snowwars/application/RunTest.java
+++ test/ch/hsr/se2p/snowwars/application/RunTest.java
@@ -1,17 +1,16 @@
 package ch.hsr.se2p.snowwars.application;
 
-import org.easymock.EasyMock;
 import org.junit.Test;
-
-import ch.hsr.se2p.snowwars.config.SnowWarsConfig;
 
 public class RunTest {
 
-    @Test
-    public void test() {
-//        SnowWarsClient snowWarsClientMock = EasyMock.createMock(SnowWarsClient.class);
-//        EasyMock.expect(snowWarsClientMock.getClientConfig()).andReturn(new SnowWarsConfig());
-//        EasyMock.replay(snowWarsClientMock);
-    }
+	@Test
+	public void test() {
+		// SnowWarsClient snowWarsClientMock =
+		// EasyMock.createMock(SnowWarsClient.class);
+		// EasyMock.expect(snowWarsClientMock.getClientConfig()).andReturn(new
+		// SnowWarsConfig());
+		// EasyMock.replay(snowWarsClientMock);
+	}
 
 }
