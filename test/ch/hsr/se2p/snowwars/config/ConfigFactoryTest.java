4c07f72734ea18b8ca47bb509eb45c5c26283099
Nurtak <nurtak@gmail.com>
2013-04-22 14:07:02 +0200

    testing

--- test/ch/hsr/se2p/snowwars/config/ConfigFactoryTest.java
+++ test/ch/hsr/se2p/snowwars/config/ConfigFactoryTest.java
@@ -2,13 +2,20 @@ package ch.hsr.se2p.snowwars.config;
 
 import static org.junit.Assert.*;
 
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class ConfigFactoryTest {
 
+    static SnowWarsConfig snowWarsConfig;
+
+    @BeforeClass
+    public static void setUpConfig() {
+        snowWarsConfig = ConfigFactory.getSnowWarsConfig();
+    }
+    
 	@Test
 	public void testGetSnowWarsConfig() {
-		SnowWarsConfig snowWarsConfig = ConfigFactory.getSnowWarsConfig();
         assertEquals(1099, snowWarsConfig.getRmiRegistryPort());
         assertEquals("snowwars", snowWarsConfig.getServerRMILookupName());
 	}
