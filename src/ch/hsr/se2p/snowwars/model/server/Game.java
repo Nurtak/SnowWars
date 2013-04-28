1ffdfd5e181636b9d6a984a21965342e05e99343
flae <freinhar@hsr.ch>
2013-04-28 18:21:46 +0200

    restructure

--- src/ch/hsr/se2p/snowwars/model/server/Game.java
+++ src/ch/hsr/se2p/snowwars/model/server/Game.java
@@ -2,27 +2,25 @@ package ch.hsr.se2p.snowwars.model.server;
 
 import ch.hsr.se2p.snowwars.model.Shot;
 import ch.hsr.se2p.snowwars.model.remoteinterfaces.GameInterface;
-import ch.hsr.se2p.snowwars.view.game.Board;
 
 public class Game implements GameInterface {
-    
-    private Board board;
-    
-    public Game(){
-        
-    }
-    
 
-    @Override
-    public void startBuildingSnowball() {
-        // TODO Auto-generated method stub
+	// private Board board;
 
-    }
+	public Game() {
 
-    @Override
-    public void throwShot(Shot shot) {
-        // TODO Auto-generated method stub
+	}
 
-    }
+	@Override
+	public void startBuildingSnowball() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void throwShot(Shot shot) {
+		// TODO Auto-generated method stub
+
+	}
 
 }
