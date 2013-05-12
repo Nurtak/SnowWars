e1bcc0d10b9b8040e089fc564cbf63f70e6ade08
Nurtak <nurtak@gmail.com>
2013-05-12 21:04:10 +0200

    no connection at start

--- src/ch/hsr/se2p/snowwars/network/client/StartRMIClient.java
+++ src/ch/hsr/se2p/snowwars/network/client/StartRMIClient.java
@@ -9,59 +9,69 @@ import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 import java.util.Observable;
 
+import javax.swing.JOptionPane;
+
 import org.apache.log4j.Logger;
 
 import ch.hsr.se2p.snowwars.config.Config;
 import ch.hsr.se2p.snowwars.config.ConfigLoader;
+import ch.hsr.se2p.snowwars.exceptions.SnowWarsRMIException;
 import ch.hsr.se2p.snowwars.network.server.RMIServerInterface;
 import ch.hsr.se2p.snowwars.network.session.server.ConnectedServerSessionInterface;
 
 public class StartRMIClient extends Observable {
 
-	private final static Logger logger = Logger.getLogger(StartRMIClient.class.getPackage().getName());
-	private final Config config;
-	private ConnectedServerSessionInterface connectedServerSessionInterface;
-	private RMIServerInterface server;
+    private final static Logger logger = Logger.getLogger(StartRMIClient.class.getPackage().getName());
+    private final Config config;
+    private ConnectedServerSessionInterface connectedServerSessionInterface;
+    private RMIServerInterface server;
 
-	public StartRMIClient() {
-		config = ConfigLoader.getConfig();
-		setRMIPropertyAndSecurity();
-		setServer();
-		setConnectedServerSessionInterface();
-	}
+    public StartRMIClient() {
+        config = ConfigLoader.getConfig();
+        setRMIPropertyAndSecurity();
+        try {
+            setServer();
+            setConnectedServerSessionInterface();
+        } catch (SnowWarsRMIException e) {
+            JOptionPane.showMessageDialog(null, "No connection to server!", ":(", JOptionPane.ERROR_MESSAGE);
+            System.exit(0);
+        }
+    }
 
-	private void setRMIPropertyAndSecurity() {
-		try {
-			System.setProperty("java.security.policy", "rmi.policy");
-			System.setProperty("java.rmi.system.hostname", InetAddress.getLocalHost().getHostAddress());
-			if (System.getSecurityManager() == null) {
-				System.setSecurityManager(new RMISecurityManager());
-			}
-		} catch (UnknownHostException e) {
-			logger.error(e.getMessage());
-		}
-	}
-	
-	private void setServer() {
-		try {
-			Registry serverRegistry = LocateRegistry.getRegistry(config.getHostname(), config.getRmiRegistryPort());
-			server = (RMIServerInterface) serverRegistry.lookup(config.getServerRMILookupName());
-		} catch (RemoteException | NotBoundException e) {
-			logger.error(e.getMessage());
-		}
-	}
+    private void setRMIPropertyAndSecurity() {
+        try {
+            System.setProperty("java.security.policy", "rmi.policy");
+            System.setProperty("java.rmi.system.hostname", InetAddress.getLocalHost().getHostAddress());
+            if (System.getSecurityManager() == null) {
+                System.setSecurityManager(new RMISecurityManager());
+            }
+        } catch (UnknownHostException e) {
+            logger.error(e.getMessage());
+        }
+    }
 
-	private void setConnectedServerSessionInterface() {
-		try {
-			connectedServerSessionInterface = server.connect();
-			logger.info("Successfully Connected to " + config.getHostname());
-		} catch (RemoteException e) {
-			logger.error(e.getMessage());
-		}
-	}
+    private void setServer() throws SnowWarsRMIException {
+        try {
+            Registry serverRegistry = LocateRegistry.getRegistry(config.getHostname(), config.getRmiRegistryPort());
+            server = (RMIServerInterface) serverRegistry.lookup(config.getServerRMILookupName());
+        } catch (RemoteException | NotBoundException e) {
+            logger.error(e.getMessage());
+            throw new SnowWarsRMIException(e.getMessage());
+        }
+    }
 
-	public ConnectedServerSessionInterface getConnectedServerSessionInterface() {
-		return connectedServerSessionInterface;
-	}
-	
+    private void setConnectedServerSessionInterface() throws SnowWarsRMIException {
+        try {
+            connectedServerSessionInterface = server.connect();
+            logger.info("Successfully Connected to " + config.getHostname());
+        } catch (RemoteException e) {
+            logger.error(e.getMessage());
+            throw new SnowWarsRMIException(e.getMessage());
+        }
+    }
+
+    public ConnectedServerSessionInterface getConnectedServerSessionInterface() {
+        return connectedServerSessionInterface;
+    }
+
 }
