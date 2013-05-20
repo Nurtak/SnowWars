0f827605a47242bffedf64d076c7f34d8b02927f
flae <flae@flaelaptop>
2013-05-20 20:15:21 +0200

    ...and is displayed!

---
+++ src/ch/hsr/se2p/snowwars/view/game/WeightValueBar.java
@@ -0,0 +1,44 @@
+package ch.hsr.se2p.snowwars.view.game;
+
+import java.awt.Color;
+import java.awt.Graphics2D;
+
+import ch.hsr.se2p.snowwars.model.Player;
+import ch.hsr.se2p.snowwars.model.Player.PlayerPosition;
+
+public class WeightValueBar {
+
+	private PlayerPosition position;
+	private final double weightValue;
+
+	private final static int Y_DISTANCE_TO_PLAYER = 200;
+	//private final static int PANEL_MARGIN = 10;
+
+	//private final static String INFO_MESSAGE = "(affects damage and flight)";
+	private final static String WEIGHT_STRING = "Weight: ";
+
+	public WeightValueBar(PlayerPosition playerPosition, double weightValue) {
+		this.position = playerPosition;
+		this.weightValue = weightValue;
+	}
+
+	public void paint(Graphics2D g2d) {
+		String weightString = WEIGHT_STRING + weightValue;
+
+		int xPosition = 0;
+		int yPosition = 0;
+		switch (position) {
+			case LEFT :
+				xPosition = Player.PLAYER_LEFT_POSITION_X;
+				yPosition = Player.PLAYER_LEFT_POSITION_Y + Y_DISTANCE_TO_PLAYER;
+				break;
+			case RIGHT :
+				xPosition = Player.PLAYER_RIGHT_POSITION_X;
+				yPosition = Player.PLAYER_RIGHT_POSITION_Y + Y_DISTANCE_TO_PLAYER;
+				break;
+		}
+
+		g2d.setColor(Color.BLACK);
+		g2d.drawString(weightString, xPosition, yPosition);
+	}
+}
