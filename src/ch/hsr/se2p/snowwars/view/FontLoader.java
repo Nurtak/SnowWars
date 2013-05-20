51985a4de434a166950839c3472a2d8fb0333a2c
mstaub <martina@192.168.1.9>
2013-05-20 15:36:38 +0200

    fontloader

---
+++ src/ch/hsr/se2p/snowwars/view/FontLoader.java
@@ -0,0 +1,28 @@
+package ch.hsr.se2p.snowwars.view;
+
+import java.awt.Font;
+import java.awt.FontFormatException;
+import java.io.File;
+import java.io.IOException;
+
+public class FontLoader {
+
+	private static final String FONT_PATH = "fonts/";
+
+	public static final Font OrangeJuice;
+
+	static {
+		Font tryFont = null;
+		try {
+			tryFont = Font.createFont(Font.TRUETYPE_FONT, new File(FONT_PATH + "orangejuice.ttf"));
+		} catch (FontFormatException e) {
+			throw new ExceptionInInitializerError(e);
+		} catch (IOException e) {
+			throw new ExceptionInInitializerError(e);
+		} finally {
+			OrangeJuice = tryFont.deriveFont(Font.BOLD, 35);
+
+		}
+	}
+
+}
