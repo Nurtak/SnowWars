4987dd38558e4d02596e8740f41e8ed19bf02e77
mstaub <martina@192.168.1.9>
2013-05-23 19:04:22 +0200

    hfhjjhg

--- src/ch/hsr/se2p/snowwars/view/BufferedImageLoader.java
+++ src/ch/hsr/se2p/snowwars/view/BufferedImageLoader.java
@@ -12,7 +12,7 @@ public class BufferedImageLoader {
 	private final static String FILENAME_THROW_PLAYER_RIGHT = new String("/img/ThrowRightPlayer.png");
 	private final static String FILENAME_SPRITESHEET_SNOWBALL = new String("/img/snowball.png");
 	private final static String FILENAME_SPRITESHEET_KNOLL = new String("/img/Knoll.png");
-	private final static String FILENAME_BENDDOWN_PLAYER_LEFT = new String("/img/BendDownLeftPlayer2");
+	private final static String FILENAME_BENDDOWN_PLAYER_LEFT = new String("/img/BendDownLeftPlayer2.png");
 	
 	private static BufferedImageLoader bil;
 
