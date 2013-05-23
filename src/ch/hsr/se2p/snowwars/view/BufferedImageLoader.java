19c69bb4a959b5a019a5caa060b18287100bec77
Nurtak <nurtak@gmail.com>
2013-05-23 21:21:32 +0200

    testing and refactoring bufferedimageloader

--- src/ch/hsr/se2p/snowwars/view/BufferedImageLoader.java
+++ src/ch/hsr/se2p/snowwars/view/BufferedImageLoader.java
@@ -6,55 +6,56 @@ import java.io.IOException;
 import javax.imageio.ImageIO;
 
 public class BufferedImageLoader {
-	private final static String FILENAME_BACKGROUND = new String("/img/background.jpg");
-	private final static String FILENAME_LOGO = new String("/img/logo.png");
-	private final static String FILENAME_THROW_PLAYER_LEFT = new String("/img/ThrowLeftPlayer.png");
-	private final static String FILENAME_THROW_PLAYER_RIGHT = new String("/img/ThrowRightPlayer.png");
-	private final static String FILENAME_SPRITESHEET_SNOWBALL = new String("/img/snowball.png");
-	private final static String FILENAME_SPRITESHEET_KNOLL = new String("/img/Knoll.png");
-	private final static String FILENAME_BENDDOWN_PLAYER_LEFT = new String("/img/BendDownLeftPlayer2.png");
-	
-	private static BufferedImageLoader bil;
+    private final static String IMAGE_PATH = "/img/";
+    private final static String FILENAME_BACKGROUND = IMAGE_PATH + "background.jpg";
+    private final static String FILENAME_LOGO = IMAGE_PATH + "logo.png";
+    private final static String FILENAME_THROW_PLAYER_LEFT = IMAGE_PATH + "ThrowLeftPlayer.png";
+    private final static String FILENAME_THROW_PLAYER_RIGHT = IMAGE_PATH + "ThrowRightPlayer.png";
+    private final static String FILENAME_SPRITESHEET_SNOWBALL = IMAGE_PATH + "snowball.png";
+    private final static String FILENAME_SPRITESHEET_KNOLL = IMAGE_PATH + "Knoll.png";
+    private final static String FILENAME_BENDDOWN_PLAYER_LEFT = IMAGE_PATH + "BendDownLeftPlayer2.png";
+    
+    private static BufferedImageLoader instance = null;
 
-	private BufferedImageLoader() {
-	}
+    private BufferedImageLoader() {
+    }
 
-	public static BufferedImageLoader getInstance() {
-		if (bil == null) {
-			bil = new BufferedImageLoader();
-		}
-		return bil;
-	}
+    public static BufferedImageLoader getInstance() {
+        if (instance == null) {
+            instance = new BufferedImageLoader();
+        }
+        return instance;
+    }
 
-	private BufferedImage loadImage(String pathRelativeToThis) throws IOException {
-		return ImageIO.read(BufferedImage.class.getResourceAsStream(pathRelativeToThis));
-	}
+    private BufferedImage loadImage(String pathRelativeToThis) throws IOException {
+        return ImageIO.read(BufferedImage.class.getResourceAsStream(pathRelativeToThis));
+    }
 
-	public BufferedImage getBendDownLeftPlayer() throws IOException{
-		return loadImage(FILENAME_BENDDOWN_PLAYER_LEFT);
-	}
-	
-	public BufferedImage getSnowballSpriteSheet() throws IOException {
-		return loadImage(FILENAME_SPRITESHEET_SNOWBALL);
-	}
+    public BufferedImage getBendDownLeftPlayer() throws IOException{
+        return loadImage(FILENAME_BENDDOWN_PLAYER_LEFT);
+    }
+    
+    public BufferedImage getSnowballSpriteSheet() throws IOException {
+        return loadImage(FILENAME_SPRITESHEET_SNOWBALL);
+    }
 
-	public BufferedImage getBackgroundImage() throws IOException {
-		return loadImage(FILENAME_BACKGROUND);
-	}
+    public BufferedImage getBackgroundImage() throws IOException {
+        return loadImage(FILENAME_BACKGROUND);
+    }
 
-	public BufferedImage getLogoImage() throws IOException {
-		return loadImage(FILENAME_LOGO);
-	}
+    public BufferedImage getLogoImage() throws IOException {
+        return loadImage(FILENAME_LOGO);
+    }
 
-	public BufferedImage getPlayerLeftSpriteSheet() throws IOException {
-		return loadImage(FILENAME_THROW_PLAYER_LEFT);
-	}
+    public BufferedImage getPlayerLeftSpriteSheet() throws IOException {
+        return loadImage(FILENAME_THROW_PLAYER_LEFT);
+    }
 
-	public BufferedImage getPlayerRightSpriteSheet() throws IOException {
-		return loadImage(FILENAME_THROW_PLAYER_RIGHT);
-	}
+    public BufferedImage getPlayerRightSpriteSheet() throws IOException {
+        return loadImage(FILENAME_THROW_PLAYER_RIGHT);
+    }
 
-	public BufferedImage getKnollSpriteSheet() throws IOException {
-		return loadImage(FILENAME_SPRITESHEET_KNOLL);
-	}
+    public BufferedImage getKnollSpriteSheet() throws IOException {
+        return loadImage(FILENAME_SPRITESHEET_KNOLL);
+    }
 }
