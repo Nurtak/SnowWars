e286120f2af919589649049a413392ab1369789c
mstaub <martina@192.168.1.9>
2013-05-25 16:58:31 +0200

    fhgag

--- src/ch/hsr/se2p/snowwars/view/FontLoader.java
+++ src/ch/hsr/se2p/snowwars/view/FontLoader.java
@@ -7,20 +7,21 @@ import java.io.IOException;
 
 public class FontLoader {
 
-	private static final String FONT_PATH = "fonts/";
+	private static final String FONT_PATH = "/fonts/fonts/";
 
 	public static final Font OrangeJuice;
+	
 
 	static {
 		Font tryFont = null;
 		try {
-			tryFont = Font.createFont(Font.TRUETYPE_FONT, new File(FONT_PATH + "orangejuice.ttf"));
+			tryFont = Font.createFont(Font.TRUETYPE_FONT, new File(FONT_PATH + "Feed The Bears.ttf"));
 		} catch (FontFormatException e) {
 			throw new ExceptionInInitializerError(e);
 		} catch (IOException e) {
 			throw new ExceptionInInitializerError(e);
 		} finally {
-			OrangeJuice = tryFont.deriveFont(Font.BOLD, 35);
+			OrangeJuice = tryFont.deriveFont(Font.PLAIN, 35);
 
 		}
 	}
--- src/ch/hsr/se2p/snowwars/view/game/presentation/PlayerInfoPanel.java
+++ src/ch/hsr/se2p/snowwars/view/game/presentation/PlayerInfoPanel.java
@@ -3,19 +3,21 @@ package ch.hsr.se2p.snowwars.view.game.presentation;
 import java.awt.Color;
 import java.awt.Font;
 import java.awt.Graphics2D;
+import java.awt.RenderingHints;
 
 import ch.hsr.se2p.snowwars.model.Player;
 import ch.hsr.se2p.snowwars.model.Player.PlayerPosition;
+import ch.hsr.se2p.snowwars.view.FontLoader;
 
 public class PlayerInfoPanel {
 
 	private final static int PANEL_WIDTH = 300;
 	private final static int PANEL_MARGIN = 10;
-	private final static int POSITION_Y = 20;
+	private final static int POSITION_Y = 40;
 	private final static int HEALTH_BAR_HEIGHT = 20;
 
-	private final static Font USERNAME_FONT = new Font("Arial", Font.PLAIN, 16);
-	private final static Color FONT_COLOR = Color.BLACK;
+	private static Font usernameFont = FontLoader.OrangeJuice;
+	private final static Color FONT_COLOR = Color.WHITE;
 	private final static Color GREEN = new Color(23, 109, 12);
 	private final static Color RED = new Color(198, 17, 17);
 	private final static Color ORANGE = new Color(246, 94, 2);
@@ -29,6 +31,7 @@ public class PlayerInfoPanel {
 		this.GAME_WIDTH = gameWidth;
 		this.position = pos;
 		this.player = player;
+	
 	}
 
 	public void paint(Graphics2D g2d) {
@@ -46,9 +49,9 @@ public class PlayerInfoPanel {
 				int stringLength = (int) g2d.getFontMetrics().getStringBounds(player.getUser().getName(), g2d).getWidth();
 				xPosition = GAME_WIDTH - PANEL_MARGIN - stringLength;
 		}
-
-		g2d.setFont(USERNAME_FONT);
+		g2d.setFont(usernameFont);
 		g2d.setColor(FONT_COLOR);
+		g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
 		g2d.drawString(player.getUser().getName(), xPosition, POSITION_Y);
 	}
 
