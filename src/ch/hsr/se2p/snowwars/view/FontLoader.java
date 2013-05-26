ea2a48a7027b26854785472d09447630541aae6f
flae <flae@flaelaptop>
2013-05-26 22:23:22 +0200

    asdfdsaf

--- src/ch/hsr/se2p/snowwars/view/FontLoader.java
+++ src/ch/hsr/se2p/snowwars/view/FontLoader.java
@@ -2,10 +2,8 @@ package ch.hsr.se2p.snowwars.view;
 
 import java.awt.Font;
 import java.awt.FontFormatException;
-import java.io.File;
 import java.io.IOException;
-import java.net.URISyntaxException;
-import java.net.URL;
+import java.io.InputStream;
 
 import org.apache.log4j.Logger;
 
@@ -16,37 +14,34 @@ public class FontLoader {
 	private static final String FONT_PATH = "/fonts/";
 	private static final String FONT_FILE_PATH = FONT_PATH + "PreppyGirlsHandwriting.ttf";
 	private Font gameFont = null;
-	
-	private static FontLoader instance = null;
-    private FontLoader() {
-    }
 
-    public static FontLoader getInstance() {
-        if (instance == null) {
-            instance = new FontLoader();
-        }
-        return instance;
-    }
-	
-    public Font getGameFont(int size){
-    	initFont();
-    	return gameFont.deriveFont(Font.PLAIN, size);
-    }
-    
-    private void initFont(){
-      	if(gameFont == null){
-    		try {
-    			URL fontURL = FontLoader.class.getResource(FONT_FILE_PATH);
-    			File fontFile = new File(fontURL.toURI());
-    			gameFont = Font.createFont(Font.TRUETYPE_FONT, fontFile);
-    			logger.info("Successfully loaded font from " + FONT_FILE_PATH);
-    		} catch (FontFormatException e) {
-    			logger.error(e.getMessage(), e);
-    		} catch (IOException e) {
-    			logger.error(e.getMessage(), e);
-    		} catch (URISyntaxException e) {
-    			logger.error(e.getMessage(), e);
-    		}
-    	}
-    }
+	private static FontLoader instance = null;
+	private FontLoader() {
+	}
+
+	public static FontLoader getInstance() {
+		if (instance == null) {
+			instance = new FontLoader();
+		}
+		return instance;
+	}
+
+	public Font getGameFont(int size) {
+		initFont();
+		return gameFont.deriveFont(Font.PLAIN, size);
+	}
+
+	private void initFont() {
+		if (gameFont == null) {
+			try {
+				InputStream fontStream = FontLoader.class.getResourceAsStream(FONT_FILE_PATH);
+				gameFont = Font.createFont(Font.TRUETYPE_FONT, fontStream);
+				logger.info("Successfully loaded font from " + FONT_FILE_PATH);
+			} catch (FontFormatException e) {
+				logger.error(e.getMessage(), e);
+			} catch (IOException e) {
+				logger.error(e.getMessage(), e);
+			}
+		}
+	}
 }
