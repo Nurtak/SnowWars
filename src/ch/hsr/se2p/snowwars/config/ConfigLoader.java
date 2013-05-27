3ab2676ef3435fdcae1d74485340aece0a3cb872
flae <flae@flaelaptop>
2013-05-27 13:20:12 +0200

    asdf

--- src/ch/hsr/se2p/snowwars/config/ConfigLoader.java
+++ src/ch/hsr/se2p/snowwars/config/ConfigLoader.java
@@ -1,5 +1,12 @@
 package ch.hsr.se2p.snowwars.config;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+import org.apache.commons.configuration.XMLConfiguration;
 import org.apache.log4j.Logger;
 
 public class ConfigLoader {
@@ -8,39 +15,33 @@ public class ConfigLoader {
 
 	public static Config getConfig() {
 		Config snowWarsConfig;
-		// try {
-		logger.info("Load XML config...");
+		try {
+			logger.info("Load XML config...");
 
-		// File configfile = new File("config.xml");
-		// if (!configfile.exists()) {
-		// int readBytes;
-		// byte[] buffer = new byte[4096];
-		// try (InputStream stream =
-		// ConfigLoader.class.getResourceAsStream("/config.xml"); OutputStream
-		// resStreamOut = new FileOutputStream(configfile)) {
-		// while ((readBytes = stream.read(buffer)) > 0) {
-		// resStreamOut.write(buffer, 0, readBytes);
-		// }
-		// } catch (IOException e1) {
-		// e1.printStackTrace();
-		// }
-		// }
+			File configfile = new File("config.xml");
+			if (!configfile.exists()) {
+				int readBytes;
+				byte[] buffer = new byte[4096];
+				try (InputStream stream = ConfigLoader.class.getResourceAsStream("/config.xml"); OutputStream resStreamOut = new FileOutputStream(configfile)) {
+					while ((readBytes = stream.read(buffer)) > 0) {
+						resStreamOut.write(buffer, 0, readBytes);
+					}
+				} catch (IOException e1) {
+					e1.printStackTrace();
+				}
+			}
 
-		// XMLConfiguration xmlConfig = new XMLConfiguration("config.xml");
+			XMLConfiguration xmlConfig = new XMLConfiguration("config.xml");
 
-		snowWarsConfig = new Config("152.96.236.2", "snowwars", 1099, 1098);
+			snowWarsConfig = new Config("152.96.236.2", "snowwars", 1099, 1098);
 
-		// snowWarsConfig = new Config(xmlConfig.getString("hostname"),
-		// xmlConfig.getString("lookupname"),
-		// xmlConfig.getInt("port.rmi_registry"),
-		// xmlConfig.getInt("port.rmi_remote"));
-		logger.info("XML config: OK");
-		return snowWarsConfig;
-
-		// } catch (ConfigurationException cex) {
-		// logger.error(cex.getMessage());
-		// return null;
-		// }
+			snowWarsConfig = new Config(xmlConfig.getString("hostname"), xmlConfig.getString("lookupname"), xmlConfig.getInt("port.rmi_registry"), xmlConfig.getInt("port.rmi_remote"));
+			logger.info("XML config: OK");
+			return snowWarsConfig;
+		} catch (org.apache.commons.configuration.ConfigurationException e) {
+			logger.error(e.getMessage(), e);
+			return null;
+		}
 	}
 
 }
